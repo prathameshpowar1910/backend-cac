b5fbb33dfb84ee97e4e6fa9326e3bf3968979688
Prathamesh Powar <ppowar1910@gmail.com>
2023-11-04 18:19:25 +0530

    add express boilerplate

---
+++ 01_basic/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("This is Get");
+});
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server is running on port ${port}`);
+});
