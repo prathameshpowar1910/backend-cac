ec675b372e984c1ed820088cffc12856f8fb6bb6
Prathamesh Powar <ppowar1910@gmail.com>
2023-11-05 01:26:13 +0530

    Add axios and display superheroes in App component

--- 02_bewithfe/frontend/src/App.jsx
+++ 02_bewithfe/frontend/src/App.jsx
@@ -1,12 +1,33 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [superheroes, setSuperheroes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/superheroes")
+      .then((response) => {
+        setSuperheroes(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
   return (
     <>
-      <h1> Chai with Backend{'>'}Frontend </h1>
+      <h1> Chai with Backend{">>>>"}Frontend </h1>
+      <p>Superheroes : {superheroes.length} </p>
+      {superheroes.map((superhero) => (
+        <div key={superhero.id}>
+          <h3>{superhero.name}</h3>
+          <p>{superhero.universe}</p>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
