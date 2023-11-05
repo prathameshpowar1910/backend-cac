c8a68350669f53139c7ef4d58bd5ec005c948040
Prathamesh Powar <ppowar1910@gmail.com>
2023-11-05 18:43:57 +0530

    Refactor user schema in sub_todo.models.js

--- 03_datamodelling/models/todos/sub_todo.models.js
+++ 03_datamodelling/models/todos/sub_todo.models.js
@@ -1,4 +1,33 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({})
+//? Simpler way to create a schema
+// const userSchema = new mongoose.Schema({
+//   username: String,
+//   email: String,
+//   isActive: Boolean,
+// });
 
+//? Professional way to create a schema
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
