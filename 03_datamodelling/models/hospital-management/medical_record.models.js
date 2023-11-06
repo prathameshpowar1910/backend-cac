972c5999669271b2aab9865bad28092475545861
Prathamesh Powar <ppowar1910@gmail.com>
2023-11-06 22:30:53 +0530

    Add patient and doctor fields to medical record schema

--- 03_datamodelling/models/hospital-management/medical_record.models.js
+++ 03_datamodelling/models/hospital-management/medical_record.models.js
@@ -1,6 +1,17 @@
 import mongoose from "mongoose";
 
-const medicalRecordSchema = new mongoose.Schema({}, { timestamps: true });
+const medicalRecordSchema = new mongoose.Schema({
+  patient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'Patient',
+    required: true,
+  },
+  doctor: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Doctor',
+    required: true,
+  },
+}, { timestamps: true });
 
 export const MedicalRecord = mongoose.model(
   "MedicalRecord",
