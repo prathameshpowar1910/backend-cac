d1155f208f22605ce78f7e1f482d8792a6a824f9
Prathamesh Powar <ppowar1910@gmail.com>
2023-11-07 13:05:12 +0530

    Add MongoDB connection and error handling to server setup

--- 04_megaproject/src/index.js
+++ 04_megaproject/src/index.js
@@ -3,20 +3,18 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-  path:"./env"
-})
+  path: "./env",
+});
 
-connectDB();
-
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MONGODB connection error !! ", error);
+  });
 
 
 
