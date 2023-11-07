f1dcfed57dc398bc9aa2be6fdd841e10be87d180
Prathamesh Powar <ppowar1910@gmail.com>
2023-11-07 13:28:28 +0530

    Add CORS and cookie-parser middleware to app.js

---
+++ 04_megaproject/src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser"; //? cookie-parser is used to parse cookies from the request headers
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    cerdentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParse());
+
+export { app };
