3e0dd77dc13f02345adc6a77e5398e44752f837e
Prathamesh Powar <ppowar1910@gmail.com>
2023-11-12 22:48:34 +0530

    Add username, email, fullname, avatar, coverImage, watchHistory, password, refreshToken fields to user model and add videoFile, thumbnail, title, description, duration, views, isPublished, owner fields to video model.

--- 04_megaproject/src/models/user.model.js
+++ 04_megaproject/src/models/user.model.js
@@ -1,5 +1,50 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
 export const User = mongoose.model("User", userSchema);
