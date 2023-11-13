7e83b2c5cac94c4ab4cdec4f8c7db5ce357f2600
Prathamesh Powar <ppowar1910@gmail.com>
2023-11-13 17:40:09 +0530

    Add JWT and bcrypt for password hashing and verification

--- 04_megaproject/src/models/user.model.js
+++ 04_megaproject/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -46,5 +48,19 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+//? here we are using a normal function because we need to use the this keyword and arrow functions don't have a this keyword
+//? here next is used to tell mongoose that we are done with this middleware and it can move on to the next middleware
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10)
+  next()
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password,this.password)
+} 
+
 
 export const User = mongoose.model("User", userSchema);
