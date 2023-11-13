459d0abbfe72dde98b0e4064fc5603d7e249b93b
Prathamesh Powar <ppowar1910@gmail.com>
2023-11-13 18:34:21 +0530

    Add password hashing and token generation methods to User model

--- 04_megaproject/src/models/user.model.js
+++ 04_megaproject/src/models/user.model.js
@@ -54,13 +54,40 @@ const userSchema = new mongoose.Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10)
-  next()
+  this.password = bcrypt.hash(this.password, 10);
+  next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password,this.password)
-} 
+  return await bcrypt.compare(password, this.password);
+};
 
+userSchema.methods.generateAccessToken = function () {
+  //? can use async here as well
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
