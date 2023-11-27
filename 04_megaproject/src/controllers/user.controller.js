7ae70b44afb44177b7c1aa8fe836bb10bc2ce247
Prathamesh Powar <ppowar1910@gmail.com>
2023-11-27 22:43:04 +0530

    Add user registration functionality

--- 04_megaproject/src/controllers/user.controller.js
+++ 04_megaproject/src/controllers/user.controller.js
@@ -1,9 +1,19 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
+  // get user details from frontend
+  // validation
+  // check if user exists
+  // check for images,check for avatar
+  // upload to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and request token fields from response
+  // check for user creation
+  // send response
+
+  const {fullName, email, username, password} = req.body
+
+  console.log("email : ", email)
 });
 
 export { registerUser };
