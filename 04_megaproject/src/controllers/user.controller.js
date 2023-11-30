22cbe65e924cbcbb3f979cb9f5b799c97404c6a6
Prathamesh Powar <ppowar1910@gmail.com>
2023-11-30 11:13:09 +0530

    add image local paths

--- 04_megaproject/src/controllers/user.controller.js
+++ 04_megaproject/src/controllers/user.controller.js
@@ -33,6 +33,15 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "User already exists");
   }
 
+  const avatarLocalPath = req.files?.avatar[0].path;
+  const coverImageLocalPath = req.files?.coverImage[0].path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  
+
 });
 
 export { registerUser };
