b9056d9fc4bbed576720754ffdd42732081b270d
Prathamesh Powar <ppowar1910@gmail.com>
2023-12-01 21:09:41 +0530

    Add user creation response

--- 04_megaproject/src/controllers/user.controller.js
+++ 04_megaproject/src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -48,14 +49,28 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(500, "Error while uploading avatar");
   }
 
-  User.create({
+  const user = await User.create({
     fullName,
     avatar: avatar.url,
     coverImage: coverImage?.url || "",
     email,
     password,
     username: username.toLowerCase(), // convert to lowercase
-  })
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Error while creating user");
+  }
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(201, createdUser, "User created successfully")
+    );
 });
 
 export { registerUser };
